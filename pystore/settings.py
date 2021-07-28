078873e2f32d93ef7616ede7fde8b2cde5b68900
Wesley <wesleygurgel27@gmail.com>
2021-07-28 11:11:10 -0300

    add pages app

--- pystore/settings.py
+++ pystore/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'pystore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # USER MODEL
